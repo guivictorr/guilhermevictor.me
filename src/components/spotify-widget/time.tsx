c52d8b6e3868e2755ed500f4a26dee155d25e8ea
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-11-12 20:20:57 -0300

    fix: removing prop dateTime from time tag

--- src/components/spotify-widget/time.tsx
+++ src/components/spotify-widget/time.tsx
@@ -4,12 +4,5 @@ import { formatRelative } from 'date-fns';
 
 export const Time = ({ dateTime }: { dateTime: string }) => {
   const formattedDate = formatRelative(new Date(dateTime), Date.now());
-  return (
-    <time
-      className='text-secondary text-sm mt-0.5'
-      dateTime={new Date(dateTime).toString()}
-    >
-      {formattedDate}
-    </time>
-  );
+  return <time className='text-secondary text-sm mt-0.5'>{formattedDate}</time>;
 };
