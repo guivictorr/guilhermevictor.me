13f3bf4d2b84d92d8930c01ed0c42da70b8b1de1
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-08 20:12:06 -0300

    feat: adding last updated spotify song

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,21 +1,25 @@
 import { getLatestPlayedTrack } from '@/services/spotify';
 import { Play } from './play';
+import { LastUpdate } from './last-update';
 
 export const Footer = async () => {
-  const { track } = await getLatestPlayedTrack();
+  const { track, played_at } = await getLatestPlayedTrack();
   const artists = track.artists.map(artist => artist.name).join(',');
 
   return (
-    <footer className='h-[52px] z-50 shrink-0 w-full border-t border-primary/10 px-4 sm:px-8'>
+    <footer className='h-16 z-50 shrink-0 w-full border-t border-primary/10 px-4 sm:px-8'>
       <div className='flex items-center justify-between gap-4 h-full'>
         <div className='flex items-center gap-4'>
           <Play audioUrl={track.preview_url} />
-          <p
-            aria-description={`Last played song ${track.name} from ${artists}`}
-            className='line-clamp-1'
-          >
-            {artists} - {track.name}
-          </p>
+          <div>
+            <LastUpdate playedAt={played_at} />
+            <p
+              aria-description={`Last played song ${track.name} from ${artists}`}
+              className='line-clamp-1'
+            >
+              {artists} - {track.name}
+            </p>
+          </div>
         </div>
         <p>{new Date().getFullYear()}</p>
       </div>
---
+++ src/components/footer/last-update.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { formatRelative } from 'date-fns';
+
+type LastUpdateProps = {
+  playedAt: string;
+};
+
+export const LastUpdate = ({ playedAt }: LastUpdateProps) => {
+  const formattedPlayedAt = formatRelative(new Date(playedAt), Date.now());
+
+  return <p className='text-xs text-secondary/40'>{formattedPlayedAt}</p>;
+};
