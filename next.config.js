ee51a262169533c5c2968fa1be76e47203180ac6
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-22 23:12:06 -0300

    chore: removing mdx things from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: true,
-  },
-};
-
-const withMDX = require('@next/mdx')();
-module.exports = withMDX(nextConfig);
+const nextConfig = {};
+export default nextConfig;
