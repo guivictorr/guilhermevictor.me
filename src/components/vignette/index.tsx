253f1e24d7720627fac61303f1b50a25da6ffcfa
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-25 18:08:18 -0300

    refactor: changing vignette color

--- src/components/vignette/index.tsx
+++ src/components/vignette/index.tsx
@@ -1,5 +1,5 @@
 export const Vignette = () => (
   <div className='fixed inset-0 pointer-events-none z-40'>
-    <div className='bg-[linear-gradient(180deg,rgba(24,15,10,.7)_0%,rgba(255,255,255,0)_20%,rgba(255,255,255,0)_50%,rgba(255,255,255,0.03125)_80%,rgba(24,15,10,.7)_100%)] w-full h-full' />
+    <div className='bg-[linear-gradient(180deg,#261911_0%,rgba(255,255,255,0)_20%,rgba(255,255,255,0)_50%,rgba(255,255,255,0.03125)_80%,#261911_100%)] w-full h-full' />
   </div>
 );
