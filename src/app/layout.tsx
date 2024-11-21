92d3e23ebc707fc14071f520c54867a4e4ff3030
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-11-21 13:12:05 -0300

    feat: add microsoft clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import '@/styles/globals.css';
 import { buildSEO } from '@/seo/seo';
 import { Noise } from '@/components/noise';
 import { structuredData } from '@/seo/structured-data';
+import Script from 'next/script';
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
@@ -26,7 +27,18 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en' className={`${playfair.variable} ${GeistSans.variable}`}>
       <head>
-        <script
+        <Script type='text/javascript' strategy='afterInteractive'>
+          {`
+
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "p23rxyo4dc");
+`}
+        </Script>
+        <Script
+          id='STRUCTURED-DATA'
           key='structured-data'
           type='application/ld+json'
           dangerouslySetInnerHTML={{
