0df8895ae8390c980b3cc2e8227b28c07192f23d
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-11-21 13:20:44 -0300

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import { buildSEO } from '@/seo/seo';
 import { Noise } from '@/components/noise';
 import { structuredData } from '@/seo/structured-data';
 import Script from 'next/script';
+import { Scripts } from './scripts';
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
@@ -27,24 +28,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en' className={`${playfair.variable} ${GeistSans.variable}`}>
       <head>
-        <Script type='text/javascript' strategy='afterInteractive'>
-          {`
-
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "p23rxyo4dc");
-`}
-        </Script>
-        <Script
-          id='STRUCTURED-DATA'
-          key='structured-data'
-          type='application/ld+json'
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify(structuredData),
-          }}
-        />
+        <Scripts />
       </head>
       <body className='bg-background text-secondary scroll-smooth'>
         <Noise />
---
+++ src/app/scripts.tsx
@@ -0,0 +1,43 @@
+import Script from 'next/script';
+import { structuredData } from '@/seo/structured-data';
+
+export function Scripts() {
+  return (
+    <>
+      <Script
+        strategy='afterInteractive'
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-BX9KZ5K063'
+      ></Script>
+      <Script id='GOOGLE-ANALYTICS' strategy='afterInteractive'>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BX9KZ5K063');
+`}</Script>
+      <Script
+        id='MICROSOFT_CLARITY'
+        type='text/javascript'
+        strategy='afterInteractive'
+      >
+        {`
+
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "p23rxyo4dc");
+`}
+      </Script>
+      <Script
+        id='STRUCTURED-DATA'
+        key='structured-data'
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(structuredData),
+        }}
+      />
+    </>
+  );
+}
