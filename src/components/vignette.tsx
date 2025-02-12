66dc208f3b94564f9854147dc81bec69dc5aecf3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-02-12 20:06:58 -0300

    fix: fix vignette in light mode

--- src/components/vignette.tsx
+++ src/components/vignette.tsx
@@ -1,5 +1,5 @@
 export const Vignette = () => (
   <div className='fixed inset-0 pointer-events-none z-40'>
-    <div className='bg-[linear-gradient(180deg,#261911_0%,rgba(255,255,255,0)_20%,rgba(255,255,255,0)_50%,rgba(255,255,255,0)_80%,#261911_100%)] w-full h-full' />
+    <div className='vignette w-full h-full' />
   </div>
 );
