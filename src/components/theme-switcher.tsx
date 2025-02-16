fc5cdbebda75928a2d61e947ba5c877960b9d530
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-02-16 19:28:30 -0300

    refactor: improving a11y in `ThemeSwitcher` and color contrast

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,31 +1,41 @@
 'use client';
 import { useTheme } from 'next-themes';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useId } from 'react';
 import { Icon } from './icon';
 
 export function ThemeSwitcher() {
   const { theme, themes, setTheme } = useTheme();
+  const id = useId();
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
 
   return (
-    <ul className='absolute top-8 right-8 z-20 flex flex-row-reverse items-center gap-8'>
+    <fieldset className='absolute top-8 right-8 z-20 flex items-center gap-8'>
+      <legend className='sr-only'>Select a display theme:</legend>
       {mounted &&
         themes.map(t => (
-          <li key={t} className='relative'>
-            <button
-              aria-label={`Theme ${t}`}
-              className='capitalize'
-              onClick={() => setTheme(t)}
-            >
+          <span
+            key={t}
+            className='p-1 relative has-[:focus-visible]:ring-2 ring-primary rounded-sm'
+          >
+            <label htmlFor={`${id}_${t}`} className='cursor-pointer'>
+              <span className='sr-only'>{t}</span>
               <Icon icon={t as 'system' | 'dark' | 'light'} />
-            </button>
-
-            {t === theme && (
-              <div className='absolute size-1 bg-primary rounded-full left-1/2 -translate-x-1/2'></div>
-            )}
-          </li>
+              {t === theme && (
+                <div className='absolute size-1 bg-primary rounded-full -bottom-1 left-1/2 -translate-x-1/2'></div>
+              )}
+            </label>
+            <input
+              type='radio'
+              name='theme'
+              id={`${id}_${t}`}
+              value={t}
+              className='sr-only'
+              onChange={() => setTheme(t)}
+              checked={t === theme}
+            />
+          </span>
         ))}
-    </ul>
+    </fieldset>
   );
 }
