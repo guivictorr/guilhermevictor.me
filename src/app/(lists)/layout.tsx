09e7c6a580405428e3843ca7380ca6e9b13503b5
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-04-06 02:01:54 -0300

    refactor: add lists layout

---
+++ src/app/(lists)/layout.tsx
@@ -0,0 +1,22 @@
+import { Icon } from '@/components/icon';
+import Link from 'next/link';
+import { PropsWithChildren } from 'react';
+
+export default function ListsLayout({ children }: PropsWithChildren) {
+  return (
+    <div className='max-w-2xl mx-auto'>
+      <header>
+        <nav>
+          <Link
+            href='/'
+            className='no-underline [&>svg]:w-6 flex items-center gap-1.5 text-secondary w-fit font-serif italic'
+          >
+            <Icon icon='back' />
+            <span>Home</span>
+          </Link>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
