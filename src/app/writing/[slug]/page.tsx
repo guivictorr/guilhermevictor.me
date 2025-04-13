8f824dbf520f8aaad2670996fae64cc679fb5934
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-04-12 23:41:16 -0300

    refactor: organizing article components

--- src/app/writing/[slug]/page.tsx
+++ src/app/writing/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { MDX } from '@/components/mdx';
-import { getPost, getPosts } from '@/services/content';
+import { getPost, getPosts, MetadataOutput } from '@/services/content';
 
 import { buildSEO } from '@/app/seo';
 import { notFound } from 'next/navigation';
@@ -35,24 +35,27 @@ export default async function PostPage(props: PostPageProps) {
   const params = await props.params;
   const { slug } = params;
   const post = getPost(slug);
-  const posts = getPosts();
 
   if (!post) {
     return notFound();
   }
 
-  const nextItemIndex = posts.findIndex(p => p.slug === slug);
-  const nextItem = posts[nextItemIndex - 1];
-
-  const previousItem = posts.filter(
-    ({ metadata }) =>
-      new Date(String(metadata.publishedAt)) <
-      new Date(String(post.metadata.publishedAt)),
-  )[0];
-
   return (
     <div className='max-w-2xl mx-auto'>
-      <nav>
+      <Header metadata={post.metadata} />
+      <main>
+        <hr className='border-secondary/10 my-8' />
+        <MDX source={post.content} />
+      </main>
+      <Footer slug={slug} />
+    </div>
+  );
+}
+
+function Header({ metadata }: { metadata: MetadataOutput }) {
+  return (
+    <header>
+      <nav className='mb-6'>
         <Link
           href='/'
           className='no-underline [&>svg]:w-6 flex items-center gap-1.5 text-secondary w-fit font-serif italic'
@@ -61,52 +64,54 @@ export default async function PostPage(props: PostPageProps) {
           <span>Home</span>
         </Link>
       </nav>
-      <main>
-        <header className='mt-6'>
-          <h1 className='text-3xl font-serif text-primary'>
-            {post.metadata.title}
-          </h1>
-          {post.metadata.publishedAt && (
-            <time
-              className='text-sm text-secondary'
-              dateTime={post.metadata.publishedAt.toString()}
-            >
-              {format(post.metadata.publishedAt, 'MMMM dd, yyyy')}
-            </time>
-          )}
-        </header>
-        <hr className='border-secondary/10 my-8' />
-        <MDX source={post.content} />
-        <footer className='md:pb-32 pb-16'>
-          <hr className='border-secondary/10 my-8' />
+      <h1 className='text-3xl font-serif text-primary'>{metadata.title}</h1>
+      {metadata.publishedAt && (
+        <time
+          className='text-sm text-secondary'
+          dateTime={metadata.publishedAt.toString()}
+        >
+          {format(metadata.publishedAt, 'MMMM dd, yyyy')}
+        </time>
+      )}
+    </header>
+  );
+}
 
-          <nav className='flex justify-between'>
-            {!!previousItem && (
-              <Link
-                href={String(previousItem.metadata.url)}
-                className='no-underline block'
-              >
-                <span className='text-sm text-secondary'>Previous</span>
-                <span className='block text-primary'>
-                  {previousItem.metadata.title}
-                </span>
-              </Link>
-            )}
-            <div className='grow' />
-            {!!nextItem && (
-              <Link
-                href={String(nextItem.metadata.url)}
-                className='no-underline block text-end'
-              >
-                <span className='text-sm text-secondary'>Next</span>
-                <span className='block text-primary'>
-                  {nextItem.metadata.title}
-                </span>
-              </Link>
-            )}
-          </nav>
-        </footer>
-      </main>
-    </div>
+function Footer({ slug }: { slug: string }) {
+  const posts = getPosts();
+  const currentItemIndex = posts.findIndex(p => p.slug === slug);
+  const nextItem = posts[currentItemIndex - 1];
+  const previousItem = posts[currentItemIndex + 1];
+
+  return (
+    <footer className='md:pb-32 pb-16'>
+      <hr className='border-secondary/10 my-8' />
+
+      <nav className='flex justify-between'>
+        {!!previousItem && (
+          <Link
+            href={String(previousItem.metadata.url)}
+            className='no-underline block'
+          >
+            <span className='text-sm text-secondary'>Previous</span>
+            <span className='block text-primary'>
+              {previousItem.metadata.title}
+            </span>
+          </Link>
+        )}
+        <div className='grow' />
+        {!!nextItem && (
+          <Link
+            href={String(nextItem.metadata.url)}
+            className='no-underline block text-end'
+          >
+            <span className='text-sm text-secondary'>Next</span>
+            <span className='block text-primary'>
+              {nextItem.metadata.title}
+            </span>
+          </Link>
+        )}
+      </nav>
+    </footer>
   );
 }
