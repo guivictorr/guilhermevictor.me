ca3840af9431c1d10cbea72f0b122ba1678c0164
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-04-19 13:43:54 -0300

    feat: change the text of copy button when copied

--- src/components/pre.tsx
+++ src/components/pre.tsx
@@ -1,24 +1,39 @@
 'use client';
 
-import { PropsWithChildren, useId } from 'react';
+import { PropsWithChildren, useEffect, useId, useState } from 'react';
 
 export function Pre({ children }: PropsWithChildren) {
   const id = useId();
-  async function copy() {
+  const [copied, setCopied] = useState(false);
+
+  function copy() {
     const textToCopy = document.querySelector(`#${id} #raw`)?.textContent;
     if (!textToCopy) return;
-    await navigator.clipboard.writeText(textToCopy);
+    navigator.clipboard.writeText(textToCopy).then(() => setCopied(true));
   }
+
+  useEffect(() => {
+    if (!copied) return;
+
+    const timeout = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [copied]);
 
   return (
     <pre id={id} className='relative overflow-hidden'>
+      <div className='absolute inset-0 h-full w-full z-10'></div>
       <div className='overflow-y-auto'>{children}</div>
       <button
         type='button'
         onClick={copy}
-        className='absolute top-2 right-2 px-2 py-1 text-xs text-secondary border rounded-md bg-background'
+        className='absolute z-20 top-2 right-2 px-2 py-1 text-xs text-secondary border rounded-md bg-background'
       >
-        Copy
+        {copied ? 'Copied' : 'Copy'}
       </button>
     </pre>
   );
