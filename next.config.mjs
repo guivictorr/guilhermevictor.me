ffc3ae61003ded5ed4f6e5e1d08f338268001aff
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-07-26 15:46:06 -0300

    feat: add redirect to social media

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,35 @@ const nextConfig = {
   experimental: {
     viewTransition: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/bluesky',
+        destination: 'https://bsky.app/profile/guilhermevictor.me',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/guilhermeviictor',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/guivictorr',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://x.com/oguivictor',
+        permanent: true,
+      },
+      {
+        source: '/x',
+        destination: 'https://x.com/oguivictor',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
