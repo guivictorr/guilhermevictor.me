632a8ca01641e58a8e4506f40f0aadfbae9090d9
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-10-28 18:10:14 -0300

    refactor: removes records collection and adds a link to /records

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,25 @@ import Link from 'next/link';
 
 import { getPosts } from '@/services/content';
 import { format } from 'date-fns';
-import { getDiscogsCollection } from '@/services/discogs';
-import { Shimmer } from '@/components/shimmer';
-import Image from 'next/image';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/carousel';
 
 export default async function Home() {
-  const posts = getPosts().map(post => post.metadata);
-  const records = await getDiscogsCollection();
-
-  const groupedPostsByYear = Object.groupBy(posts, post =>
-    new Date(post.publishedAt).getFullYear().toString(),
-  );
-
   return (
     <main className='space-y-8 max-w-xl mx-auto py-20 md:pt-28 px-2'>
-      <section
-        aria-label='Informações sobre Guilherme Victor'
-        className='flex items-start pb-4'
-      >
+      <section aria-label='Informações sobre Guilherme Victor'>
         <div>
           <p className='leading-tight'>Guilherme Victor</p>
-          <h1 className='flex mb-2 mt-1 flex-col text-2xl sm:text-4xl text-primary font-serif italic'>
+          <h1 className='mt-1 flex-col text-2xl sm:text-4xl text-primary font-serif italic'>
             desenvolvedor de software
           </h1>
+          <Link
+            href='/records'
+            className='no-underline flex-col text-2xl sm:text-4xl text-primary font-serif italic'
+          >
+            colecionador de discos
+          </Link>
+          <p className='mb-2 flex-col text-2xl sm:text-4xl text-primary font-serif italic'>
+            neovim user
+          </p>
           <p className='max-w-sm'>
             Você pode me encontrar no{' '}
             <ExternalLink href='https://x.com/oguivictor'>Twitter</ExternalLink>
@@ -62,79 +51,51 @@ export default async function Home() {
         </div>
       </section>
       <section aria-label='Navegação principal'>
-        <nav>
-          <ul className='flex flex-col gap-4'>
-            {Object.entries(groupedPostsByYear)
-              .reverse()
-              .map(([year, posts]) => (
-                <li key={year} className='relative'>
-                  <span className='text-sm text-secondary mb-1 block'>
-                    {year}
-                  </span>
-                  <ul>
-                    {posts?.map(post => (
-                      <li key={post.title}>
-                        <Link
-                          href={post.url ?? ''}
-                          className='flex items-center w-full gap-2 justify-between py-2 no-underline'
-                        >
-                          <span className='transition line-clamp-1'>
-                            {post.title}
-                          </span>
-                          {!!post.publishedAt && (
-                            <time
-                              dateTime={post.publishedAt.toString()}
-                              className='transition text-sm shrink-0 capitalize'
-                            >
-                              {format(post.publishedAt, 'MMMM dd')}
-                            </time>
-                          )}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-          </ul>
-        </nav>
-      </section>
-
-      <section className='space-y-4'>
-        <Carousel opts={{ loop: true, align: 'start', skipSnaps: true }}>
-          <div className='flex justify-between items-center gap-4 pb-4'>
-            <h2 className='flex mt-1 flex-col text-2xl sm:text-4xl text-primary font-serif italic'>
-              coleção de discos
-            </h2>
-
-            <div className='space-x-8'>
-              <CarouselPrevious />
-              <CarouselNext />
-            </div>
-          </div>
-          <CarouselContent className='items-center'>
-            {records.releases.map(record => (
-              <CarouselItem className='basis-1/4' key={record.id}>
-                <a
-                  href={`https://www.discogs.com/release/${record.id}`}
-                  target='_blank'
-                  className='no-underline'
-                >
-                  <Shimmer className='object-cover'>
-                    <Image
-                      src={record.basic_information.cover_image}
-                      alt={record.basic_information.title}
-                      sizes='(max-width: 768px) 20vw, 33vw'
-                      width={150}
-                      height={150}
-                    />
-                  </Shimmer>
-                </a>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
+        <Posts />
       </section>
       <Footer />
     </main>
+  );
+}
+
+async function Posts() {
+  const posts = getPosts().map(post => post.metadata);
+  const groupedPostsByYear = Object.groupBy(posts, post =>
+    new Date(post.publishedAt).getFullYear().toString(),
+  );
+  return (
+    <nav>
+      <ul className='flex flex-col gap-4'>
+        {Object.entries(groupedPostsByYear)
+          .reverse()
+          .map(([year, posts]) => (
+            <li key={year} className='relative'>
+              <span className='text-sm text-secondary mb-1 block'>{year}</span>
+              <ul>
+                {posts?.map(post => (
+                  <li key={post.title}>
+                    <Link
+                      href={post.url ?? ''}
+                      className='flex items-center w-full gap-2 justify-between py-2 no-underline'
+                    >
+                      <span className='transition line-clamp-1'>
+                        {post.title}
+                      </span>
+                      {!!post.publishedAt && (
+                        <time
+                          dateTime={post.publishedAt.toString()}
+                          className='transition text-sm shrink-0 capitalize'
+                        >
+                          {format(post.publishedAt, 'MMMM dd')}
+                        </time>
+                      )}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+      </ul>
+    </nav>
   );
 }
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,5 +1,4 @@
 import { getLatestPlayedTrack } from '@/services/lastfm';
-import { ExternalLink } from './external-link';
 import Image from 'next/image';
 
 export const Player = async () => {
@@ -10,22 +9,11 @@ export const Player = async () => {
       aria-label={`A última música que Guilehrme ouviu foi: ${track.song} by ${track.artist}`}
       className='flex gap-2 items-center'
     >
-      <Image
-        src={track.image}
-        alt={track.song}
-        width={36}
-        height={36}
-        className='sepia brightness-90 saturate-50'
-      />
+      <Image src={track.image} alt={track.song} width={36} height={36} />
       <div>
+        <p className='text-xs text-secondary'>última música tocada.</p>
         <p className='line-clamp-1 max-w-xs text-sm text-secondary'>
           {track.artist} - {track.song}
-        </p>
-        <p className='text-xs text-secondary'>
-          <span className='inline-block mr-1'>última música tocada. Por</span>
-          <ExternalLink href='https://www.last.fm/user/oguivictor'>
-            last.fm
-          </ExternalLink>
         </p>
       </div>
     </section>
